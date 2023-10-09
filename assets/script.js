e01c0fc4b02ad320d242663fc9dbca3efbc9f59a
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-10-08 23:22:58 -0500

    attempting to make past, present, future blocks by comparing to current hour

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
 var currentDate = $('#currentDay');
 var timeBlock = $('#time-block');
 var saveBttn = $('#saveBtn');
+var currentHour = dayjs().format('HH');
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(function () {
-  saveBttn.on('click', function () {
 
-    localStorage.setItem(timeBlock)
-  });
+function savePlan(event) {
+  event.preventDefault();
+  var parentElement = $(event.target).parent()[0].id
+  var parentElementId = $(event.target).parent()
+  var userInput = parentElement.children('textarea').value();
+  localStorage.setItem(parentElementId, userInput);
+}
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -18,13 +24,12 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
 
-    function savePlan(event) {
-      event.preventDefault();
-      var parentElement = $(event.target).parent()[0].id
-      var parentElementId = $(event.target).parent()
-      var userInput = parentElement.children('textarea').value();
-      localStorage.setItem(parentElementId, userInput);
-    }
+    $(function () {
+      saveBttn.on('click', function () {
+    
+        localStorage.getItem(userInput)
+      });
+  
 
     //
     // TODO: Add code to apply the past, present, or future class to each time
@@ -32,8 +37,14 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+      var parentContainer = $("div[id='hour-");
 
-
+      timeBlock.forEach(timeBlock => {
+        var timeBlockLess = parentContainer[id].replace('hour-', "");
+        if (timeBlockLess < currentHour {
+          timeBlock.addClass ()
+        })
+      })
 
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
