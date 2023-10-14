ca6f291c11a678bb1d76026af61c5cdcf1b342f2
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-10-14 16:21:52 -0500

    had to add .ready function in the beginning so function would execute when document was loaded

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,64 @@
+// Event was not occuring when page was loading so had to add .ready 
+
+$(document).ready(function () {
+
 // Starting by grabbing elements with variable
 
-var timeBlock = $('.time-block');
-var saveBttn = $('.saveBtn');
-var currentHour = dayjs().format('H');
+  var timeBlock = $('.time-block');
+  var saveBttn = $('.saveBtn');
+  var currentHour = dayjs().format('H');
 
 
 // Saving what the user inputs into local storage
 
-function savePlan(event) {
-  var parentElement = event.target.closest('.time-block');
-  var idTimeBlock = parent.Element.$('id');
-  var userInput = parentElement.querySelector('textarea').value;
-  localStorage.setItem(idTimeBlock, userInput);
-}
+  function savePlan(event) {
+    var parentElement = event.target.closest('.time-block');
+    var idTimeBlock = parent.Element.$('id');
+    var userInput = parentElement.querySelector('textarea').value;
+    localStorage.setItem(idTimeBlock, userInput);
+  }
 
 // Color of block based on past, present, future
 
-function blockColor() {
-  for (var i = 0; i < timeBlock.length; i++) {
-    var idTimeBlock = $(timeBlock[i]).data('hour');
-    if (currentHour < idTimeBlock) {
-      $(this).removeClass('past present').addClass('future');
-    } else if (currentHour == idTimeBlock) {
-      $(this).removeClass('past future').addClass('present');
-    } else {
-      $(this).removeClass('present future').addClass('past');
+  function blockColor() {
+    for (var i = 0; i < timeBlock.length; i++) {
+      var idTimeBlock = $(timeBlock[i]).data('hour');
+      if (currentHour < idTimeBlock) {
+        $(this).removeClass('past present').addClass('future');
+      } else if (currentHour == idTimeBlock) {
+        $(this).removeClass('past future').addClass('present');
+      } else {
+        $(this).removeClass('present future').addClass('past');
+      }
     }
   }
-}
   
 // Get the saved events from local storage
 
-function getEvents() {
-  for (var i = 0; i < timeBlock.length; i++) {
-    var idTimeBlock = $(timeBlock[i]).id;
-    var userInput = localStorage.getItem(idTimeBlock);
-    if (userInput !== null) {
-      timeBlock[i].querySelector('textarea').value = userInput;
+  function getEvents() {
+    for (var i = 0; i < timeBlock.length; i++) {
+      var idTimeBlock = $(timeBlock[i]).id;
+      var userInput = localStorage.getItem(idTimeBlock);
+      if (userInput !== null) {
+        timeBlock[i].querySelector('textarea').value = userInput;
+      }
     }
   }
-}
 
 // Setting current date with dayjs
 
-var currentDate = dayjs().format('dddd, MMMM D, YYYY');
-document.getElementById('currentDay').textContent = currentDate;
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  document.getElementById('currentDay').textContent = currentDate;
 
 // Calling the functions
 
-blockColor();
-getEvents();
+  blockColor();
+  getEvents();
 
 // Adding Event Listener for each time user presses each save button
 
-for (var i = 0; i < saveBttn.length; i++) {
-  saveBttn[i].addEventListener('click', saveUserInput);
-}
+  for (var i = 0; i < saveBttn.length; i++) {
+    saveBttn[i].addEventListener('click', saveUserInput);
+  }
+});
 
