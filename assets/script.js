ba5cd95eedde218d14e24789232e4ddaba6a294c
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-10-14 16:10:10 -0500

    getting items from local storage based on user's input

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ function savePlan(event) {
 
 // Color of block based on past, present, future
 
-function blockColor () {
+function blockColor() {
   for (var i = 0; i < timeBlock.length; i++) {
     var idTimeBlock = $(timeBlock[i]).data('hour');
     if (currentHour < idTimeBlock) {
@@ -30,37 +30,14 @@ function blockColor () {
   }
 }
   
+// Get the saved events from local storage
 
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-      $('#time-block').each(function () {
-        timeBlock = $(this).attr("id")
-        if (currentHour === timeBlock) {
-          $(this).removeClass("past")
-          $(this).removeClass("future")
-          $(this).addClass("present")
-        } else {
-          if (currentHour < timeBlock) {
-            $(this).removeClass("future")
-            $(this).removeClass("present")
-            $(this).addClass("past")
-          } else {
-            if (currentHour > timeBlock)
-            $(this).removeClass("present")
-            $(this).removeClass("past")
-            $(this).addClass("future")
-          }
-        }
-      })
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+function getEvents() {
+  for (var i = 0; i < timeBlock.length; i++) {
+    var idTimeBlock = $(timeBlock[i]).id;
+    var userInput = localStorage.getItem(idTimeBlock);
+    if (userInput !== null) {
+      timeBlock[i].querySelector('textarea').value = userInput;
+    }
+  }
+}
